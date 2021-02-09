7b0cd7a00a3a248107055b510570c0737bcaf992
masao takayama <m.takayama0929@gmail.com>
2021-02-09 13:31:54 +0000

    #4 タスクを削除できるようにする

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,7 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
   def create
     @task = task.new(task_params)
     if params[:back]
@@ -30,9 +31,16 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
   def show
     @task = Task.find(params[:id])
   end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: "タスクを削除しました！"
+  end
+
   private
   def task_params
     params.require(:task).permit(:title, :content)
