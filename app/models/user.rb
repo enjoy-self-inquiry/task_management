4440f4ea6778189b41b82c44df9af47a5c4584fa
masao takayama <m.takayama0929@gmail.com>
2021-03-19 14:12:12 +0000

    管理画面の実装・ユーザにロールを追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,22 @@
 class User < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   has_secure_password
   before_validation { email.downcase! }
   validates :password, presence: true, length: { minimum: 6 }
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true,
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_destroy :delete_check
+  #before_update :update_check
+
+  private
+  def delete_check
+    throw(:abort) if User.where(admin: true).count <= 1 && self.admin == true
+  end
+  #def update_check
+  #  if User.where(admin: true).count == 1 && self.admin == false
+  #    errors.add(:admin,"は、最低でも１人は必要です。")
+  #    throw(:abort)
+  #  end
+  #end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_122720) do
+ActiveRecord::Schema.define(version: 2021_03_19_051153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_122720) do
     t.integer "priority"
     t.bigint "user_id"
     t.index ["title"], name: "index_tasks_on_title", unique: true
-    t.index ["user_id"], name: "index_tasks_on_user_id", unique: true
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_122720) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
