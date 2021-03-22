c46bcf2393c0862eeb23fa9fe91c4ba46a5c4145
masao takayama <m.takayama0929@gmail.com>
2021-03-22 15:35:09 +0000

    Revert "Rspecの修正"
    This reverts commit 0a30fcc06fb4a55c4274d9b52e44c264807ab336.

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,37 +1,8 @@
 FactoryBot.define do
   factory :task do
-    title { 'task1' }
-    content { 'task1_content' }
+    title { 'test_title' }
+    content { 'test_content' }
     expire { '2021-12-31' }
-    priority { '高' }
-    progress { '着手中'}
-    association :user
-  end
-
-  factory :second_task, class: Task do
-    title { 'task2' }
-    content { 'task2_content' }
-    expire { '2021-12-30'}
-    priority { '中' }
-    progress { '着手中' }
-    association :user
-  end
-
-  factory :third_task, class: Task do
-    title { 'task3' }
-    content { 'task3_content' }
-    expire { '2021-12-29' }
-    priority { '低' }
-    progress { '着手中' }
-    association :user
-  end
-
-  factory :fourth_task, class: Task do
-    title { 'task4' }
-    content { 'task4_content' }
-    expire { '2021-12-30' }
-    priority { '中' }
-    progress { '完了' }
-    association :user
+    progress { '未着手'}
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,34 +3,29 @@ RSpec.describe Task, type: :model do
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
       it 'バリデーションにひっかる' do
-        task = Task.new(title: '', content: 'タイトル失敗テスト', expire: "2020-12-31")
+        task = Task.new(title: '', content: '失敗テスト')
         expect(task).not_to be_valid
       end
     end
-
     context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
-        task = Task.new(title: '詳細失敗テスト', content: '', expire: "2020-12-31")
+        task = Task.new(title: '失敗テスト', content: '')
         expect(task).not_to be_valid
       end
     end
-
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        task = FactoryBot.create(:task, title: '成功テスト', content: '成功テスト', expire: "2020-12-31")
+        task = Task.new(title: '成功テスト', content: '成功')
         expect(task).to be_valid
       end
     end
   end
-
   describe '検索機能' do
     before do
-      user = FactoryBot.create(:user, name: 'model_test', email: 'model_test@example.com')
-      @task1 = FactoryBot.create(:task, user:user)
-      @task2 = FactoryBot.create(:second_task, user:user)
-      @task3 = FactoryBot.create(:third_task, user:user)
+      @task1 = Task.create(title: 'task1', content: 'number1', expire: '2020-01-01', progress: '着手中')
+      @task2 = Task.create(title: 'task2', content: 'number2', expire: '2020-02-01', progress: '着手中')
+      @task3 = Task.create(title: 'task3', content: 'number3', expire: '2020-03-01', progress: '着手中')
     end
-
     context 'scopeメソッドでタイトルのあいまい検索をした場合' do
       it "検索キーワードを含むタスクが絞り込まれる" do
         expect(Task.search_title('task1')).to include(@task1)
